8225a8ef23e0e07e054a4a07b04f7067f10ff3d5
yashodhah <y.dominatz@gmail.com>
2023-06-11 22:20:45 +0530

    challenge 2

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,19 @@ fn main() {
     let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let base64_string = utils::base_utils::hex_to_base64(hex_string);
 
-    println!("Base64: {}", base64_string);
+    assert_eq!(base64_string, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+    //------------------
+
+    let str1 = "1c0111001f010100061a024b53535009181c";
+    let str2 = "686974207468652062756c6c277320657965";
+
+    let bytes1 = hex::decode(str1).expect("Failed to decode str1");
+    let bytes2 = hex::decode(str2).expect("Failed to decode str2");
+
+    let xor_vec = utils::base_utils::xor(&bytes1, &bytes2);
+    let xor_hex = hex::encode(xor_vec);
+
+    assert_eq!(xor_hex, "746865206b696420646f6e277420706c6179");
 }
+
+
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -11,4 +11,18 @@ pub mod base_utils {
 
         encoded
     }
+
+    pub fn xor(v1: &Vec<u8>, v2: &Vec<u8>) -> Vec<u8> {
+        if v1.len() != v2.len() {
+            panic!("Buffer lengths are unequal");
+        }
+
+        let v3: Vec<u8> = v1
+            .iter()
+            .zip(v2.iter())
+            .map(|(&x1, &x2)| x1 ^ x2)
+            .collect();
+
+        return v3;
+    }
 }
